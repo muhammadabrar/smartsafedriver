249f10a847306a77b5bc64f0a1fe1926ead39aaa
Muhammad Abrar <abrarmuhammad100@gmail.com>
2022-05-13 20:53:20 +0500

    seo update

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,7 @@ function MyApp({ Component, pageProps }) {
         ></meta>
         <meta name="description" content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
 "></meta>
+ <meta name="keywords" content="smart drivers, chauffeur service in uar, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"></meta>
         <title>Smart Safe Drivers</title>
         <link rel="icon" type="image/x-icon" href="./logo.ico"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
