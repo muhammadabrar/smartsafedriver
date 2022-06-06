29f406e24ea372c8c08d47f2f7d0eb0b1b746614
Muhammad Abrar <abrarmuhammad100@gmail.com>
2022-06-06 14:34:49 +0500

    g-tag update

--- pages/_app.js
+++ pages/_app.js
@@ -22,20 +22,20 @@ function MyApp({ Component, pageProps }) {
         ></meta>
         <meta name="description" content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
 "></meta>
- <meta name="keywords" content="smart drivers, chauffeur service in uar, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"></meta>
+ <meta name="keywords" content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"></meta>
         <title>Smart Safe Drivers</title>
         <link rel="icon" type="image/x-icon" href="./logo.ico"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <style data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap" rel="stylesheet"></style>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CVMK6WQ0Q7"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-226091029-1"></script>
 <script>
   {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-CVMK6WQ0Q7');`}
+  gtag('config', 'UA-226091029-1');`}
 </script>
       </Head>
       <body className="body">
