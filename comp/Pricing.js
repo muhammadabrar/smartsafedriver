4ff42c8d3135a09ecba927267f9a6ac739a3d6fd
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-10-16 10:36:34 +0500

    pricing qar to uae

--- comp/Pricing.js
+++ comp/Pricing.js
@@ -1,26 +1,23 @@
 import { useState } from "react";
-import {
-    CheckmarkFilled24, ChevronRight16
-  } from "@carbon/icons-react";
+import { CheckmarkFilled24, ChevronRight16 } from "@carbon/icons-react";
 export default function Pricing_comp() {
- const [phone, setphone] = useState(971544490233);
+  const [phone, setphone] = useState(971544490233);
 
-    const [showMediaIcons, setShowMediaIcons] = useState(false);
-    return (
-      <>
-
-        
-    
-<section className="services">
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <section className="services">
         <div className="wrap">
           <h1 className="section-title" style={{ marginTop: "3rem" }}>
             <span>Price</span> Structure
             {/* <hr/> */}
           </h1>
           <p style={{ fontSize: "2rem" }}>
-            Get instant price using our latest online booking system. Save time save money. Travel better.<br/>
-            We offer a very flexible pricing structure to meet the needs of all types of customers.
-
+            Get instant price using our latest online booking system. Save time
+            save money. Travel better.
+            <br />
+            We offer a very flexible pricing structure to meet the needs of all
+            types of customers.
           </p>
           <div className="price-plans">
             <div className="price-plan">
@@ -30,62 +27,175 @@ export default function Pricing_comp() {
                 <hr />
               </div>
               <>
-              <ul class="plan-points">
-										<li className="point"><i><CheckmarkFilled24 /></i>2 Hours QAR 160</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>3 Hours QAR 240</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>4 Hours QAR 320</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>5 Hours QAR 400</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>8 Hours (1 Day) QAR 500</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>16 Hours (2 Days) QAR 1000</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>24 Hours (3 Days) QAR 1500</li>
-									</ul>
-               
-                
+                <ul class="plan-points">
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    2 Hours AED 160
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    3 Hours AED 240
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    4 Hours AED 320
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    5 Hours AED 400
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    8 Hours (1 Day) AED 500
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    16 Hours (2 Days) AED 1000
+                  </li>
+                  <li className="point">
+                    <i>
+                      <CheckmarkFilled24 />
+                    </i>
+                    24 Hours (3 Days) AED 1500
+                  </li>
+                </ul>
               </>
-              <a  href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`} className="service-btn ">WhatsApp Now</a>
+              <a
+                href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`}
+                className="service-btn "
+              >
+                WhatsApp Now
+              </a>
             </div>
             <div className="price-plan active">
               <div>
                 <h1 className="plan-title">Inter-Emirates</h1>
-                <p className="plan-note">
-                Accross the UAE
-								</p>
+                <p className="plan-note">Accross the UAE</p>
                 <hr />
               </div>
               <ul class="plan-points">
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Madinat ash Shamal QAR 440</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Ar Rayyan QAR 160</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Al Wakrah 210</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Al Khawr QAR 240</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Baladiyat Umm Salal  QAR 400</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Baladiyat ar Rayyan  QAR 400</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Baladiyat ad Dawhah QAR 400</li>
-										<li className="point"><i><CheckmarkFilled24 /></i>Dubai <ChevronRight16/> Baladiyat az Za`ayin QAR 400</li>
-
-									</ul>
-              <a  href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`} className="plan-btn ">WhatsApp Now</a>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Abu Dhabi AED 440
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Sharjah AED 160
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Ajman AED 210
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> UAQ AED 240
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Rak AED 400
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Fujairah AED 400
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  Dubai <ChevronRight16 /> Al Ain AED 400
+                </li>
+              </ul>
+              <a
+                href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`}
+                className="plan-btn "
+              >
+                WhatsApp Now
+              </a>
             </div>
             <div className="price-plan">
-            <div>
+              <div>
                 <h1 className="plan-title">Within-Dubai</h1>
                 <p className="plan-note">Single Pickup Single Drop-off</p>
                 <hr />
               </div>
               <ul class="plan-points">
-									<li className="point"><i><CheckmarkFilled24 /></i>0 - 10 KM QAR 90 </li>
-									<li className="point"><i><CheckmarkFilled24 /></i>10 - 20 KM QAR 105</li>
-									<li className="point"><i><CheckmarkFilled24 /></i>20 - 30 KM QAR 120</li>
-									<li className="point"><i><CheckmarkFilled24 /></i>30 - 40 KM QAR 135</li>
-									<li className="point"><i><CheckmarkFilled24 /></i>40 - 50 KM QAR 150</li>
-									<li className="point"><i><CheckmarkFilled24 /></i>50 - 60 KM QAR 165</li>
-									<li className="point"><i><CheckmarkFilled24 /></i>60 - 70 KM QAR 180</li>
-									</ul>
-              <a  href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`} className="service-btn ">WhatsApp Now</a>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  0 - 10 KM AED 90{" "}
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  10 - 20 KM AED 105
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  20 - 30 KM AED 120
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  30 - 40 KM AED 135
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  40 - 50 KM AED 150
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  50 - 60 KM AED 165
+                </li>
+                <li className="point">
+                  <i>
+                    <CheckmarkFilled24 />
+                  </i>
+                  60 - 70 KM AED 180
+                </li>
+              </ul>
+              <a
+                href={`https://wa.me/${phone}?text=hello, I would like to book a safe driver.`}
+                className="service-btn "
+              >
+                WhatsApp Now
+              </a>
             </div>
           </div>
         </div>
       </section>
-
-       </>
-    )
-  }
+    </>
+  );
+}
