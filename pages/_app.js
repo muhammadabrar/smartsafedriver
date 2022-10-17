433f3ec7d327f65b388bc24dcebda59bd1a3d07a
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-10-17 08:36:35 +0500

    ICON

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,8 @@ function MyApp({ Component, pageProps }) {
  <meta name="keywords" content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"></meta>
         <title>Smart Safe Drivers</title>
         <link rel="icon" type="image/x-icon" href="./logo.ico"></link>
+  <link href="./logo.ico" rel="apple-touch-icon"></link>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <style data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap" rel="stylesheet"></style>
