f3d7ed66555dc36d3afbb2918ad5b53eb4bad9c0
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-10-20 13:14:36 +0500

    icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-
 import "../style/style.css";
 import "../style/nav.css";
 import "../style/hero.css";
@@ -20,28 +19,39 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
-        <meta name="description" content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
-"></meta>
- <meta name="keywords" content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"></meta>
+        <meta
+          name="description"
+          content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
+"
+        ></meta>
+        <meta
+          name="keywords"
+          content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"
+        ></meta>
         <title>Smart Safe Drivers</title>
-        <link rel="icon" type="image/x-icon" href="./logo.ico"></link>
-  <link href="./logo.ico" rel="apple-touch-icon"></link>
+        <link rel="icon" href="./logo.ico"></link>
+        <link href="./logo.ico" rel="apple-touch-icon"></link>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <style data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap" rel="stylesheet"></style>
+        <style
+          data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap"
+          rel="stylesheet"
+        ></style>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-226091029-1"></script>
-<script>
-  {`window.dataLayer = window.dataLayer || [];
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-226091029-1"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-226091029-1');`}
-</script>
+        </script>
       </Head>
       <body className="body">
-
         <Layout>
           <Component {...pageProps} />
         </Layout>
