d14c0e35cd0d1df8fd44d73d7bd32de35274dee4
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-10-20 14:36:31 +0500

    ceck

--- pages/api/_document.js
+++ pages/api/_document.js
@@ -18,11 +18,11 @@ export default function Document() {
           content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"
         ></meta>
         <title>Smart Safe Drivers</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/site.webmanifest"/>
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+<link rel="manifest" href="./site.webmanifest"/>
+<link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5"/>
 <meta name="msapplication-TileColor" content="#da532c"></meta>
 <meta name="theme-color" content="#ffffff"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
