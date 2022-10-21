d9f95de5b6d2001e541de64191215ffbf8eac360
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-10-21 06:17:12 +0500

    improved

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,52 @@ import "../style/nav.css";
 import "../style/hero.css";
 import "../style/comit.css";
 
-
-
-import App from 'next/app'
+import App from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <>
-  <head>
-  <title>Smart Safe Drivers</title>
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="description"
+          content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
+"
+        ></meta>
+        <meta
+          name="keywords"
+          content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"
+        ></meta>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+        <title>Smart Safe Drivers</title>
 
-  </head>
-  <Component {...pageProps} /></>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <style
+          data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap"
+          rel="stylesheet"
+        ></style>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-226091029-1"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-226091029-1');`}
+        </script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 // Only uncomment this method if you have blocking data requirements for
@@ -29,4 +63,4 @@ function MyApp({ Component, pageProps }) {
 //   return { ...appProps }
 // }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-        <meta
-          name="description"
-          content="Smart Safe Drivers was established with the idea of providing a comfortable travel experience to the general public, business class, tourists and others.
-"
-        ></meta>
-        <meta
-          name="keywords"
-          content="smart drivers, chauffeur service in uae, chauffeur service, chauffeur service in dubai, chauffeur service abu dhabi, chauffeur service dubai airport, After Party Pickup, uae Tour, Errand"
-        ></meta>
+       
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -36,26 +24,7 @@ export default function Document() {
         />
         <link rel="manifest" href='./site.webmanifest' />
         <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c"></meta>
-        <meta name="theme-color" content="#ffffff"></meta>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <style
-          data-href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300&display=swap"
-          rel="stylesheet"
-        ></style>
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-226091029-1"
-        ></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-226091029-1');`}
-        </script>
+        
       </Head>
 
       <body>
